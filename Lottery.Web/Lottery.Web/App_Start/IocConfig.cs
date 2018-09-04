cf7526133fa3f7619372267dd7e8ac19d0c03356
Martin <panovski62@gmail.com>
2018-09-04 21:03:52 +0200

    Implemented dependency injection and LotteryManager class as a service for checking lottery codes.

---
+++ Lottery.Web/Lottery.Web/App_Start/IocConfig.cs
@@ -0,0 +1,36 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Lottery.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Lottery.Web.App_Start
+{
+    public class IocConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterDependencies(new ContainerBuilder()));
+        }
+
+        private static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<LotteryManager>().As<ILotteryManager>().InstancePerRequest();
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Lottery.Web/Lottery.Web/Controllers/LotteryController.cs
@@ -0,0 +1,28 @@
+﻿using Lottery.Service;
+using Lottery.View.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lottery.Web.Controllers
+{
+    public class LotteryController : ApiController
+    {
+        private ILotteryManager _lotteryManager;
+
+        public LotteryController(ILotteryManager lotteryManager)
+        {
+            _lotteryManager = lotteryManager;
+        }
+
+
+        [HttpPost]
+        public AwardModel SubmitCode([FromBody] UserCodeModel userCodeModel)
+        {
+            return _lotteryManager.CheckCode(userCodeModel);
+        }
+    }
+}
